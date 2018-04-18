e6459ae0e0902c0de6c78c02bd1cb262a0efd5ad
nksharma192 <36041068+nksharma192@users.noreply.github.com>
2018-04-18 14:40:20 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      listA : ["A","B","C","D","E","F","G","H","I","J","K","L","M"],
+      listB : ["1","2","3","4"]
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <Table />
+        <Table p={this.state.listA} q={this.state.listB}/>
       </div>
     );
   }
@@ -22,6 +32,49 @@ class Header extends Component {
   }
 }
 
+function getList (a) {
+
+  var x = a.map( a => (<ul key={a}>{a}</ul>));
+  console.log("Test : " + x);
+  return x;
+}
+
+class Table extends Component {
+  render() {
+    return (
+      <div className="table-main">
+        <table>
+          <tbody>
+          <tr> 
+            <td className = "table-head"> <b> Tasks </b> </td>
+            <td className = "table-head"> <b> Completed </b> </td>
+          </tr>
+
+          <tr>
+            <td className = "table-list"> {getList(this.props.p)}
+            </td>
+            <td className = "table-list"> {getList(this.props.q)} </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+/*
+
+function lists(s,t) {
+
+  var x = s.map((n) => n);
+  var z = t.map((y) => y);
+  console.log ("X : " + x);
+  console.log ("Z : " + z);
+}
+
 class Timer extends Component {
   render() {
     return (
@@ -32,25 +85,4 @@ class Timer extends Component {
   }
 }
 
-class Table extends Component {
-  render() {
-    return (
-      <div className="table-main">
-        <table>
-          <tr> 
-            <td className = "table-head"> <b> Tasks </b> </td>
-            <td className = "table-head"> <b> Completed </b> </td>
-          </tr>
-
-          <tr>
-            <td className = "table-list"> </td>
-            <td className = "table-list"> </td>
-          </tr>
-
-        </table>
-      </div>
-    );
-  }
-}
-
-export default App;
+*/
