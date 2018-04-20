d4a3e5ab6a2a4c818808c0cfd77266a0588d4207
nksharma192 <36041068+nksharma192@users.noreply.github.com>
2018-04-20 11:48:45 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -4,19 +4,20 @@ import './App.css';
 class App extends Component {
 
   constructor(props) {
-    super(props);
-    
+    super(props);  
     this.state = {
       listA : ["A","B","C","D","E","F","G","H","I","J","K","L","M"],
       listB : ["1","2","3","4"]
-    };
-  }
+    }
+  }  
+
+  data = this.state;
 
   render() {
     return (
       <div className="App">
-        <Header />
-        <Table p={this.state.listA} q={this.state.listB}/>
+        <Header onClick={this.pushCol}/>
+        <Table listA={this.state.listA} listB={this.state.listB} move={this.moveOnClick}/> 
       </div>
     );
   }
@@ -26,17 +27,29 @@ class Header extends Component {
   render() {
     return (
       <div className="header-main">
-        <h4> <u> Todo List </u> </h4>
+        <h4 onClick={this.props.onClick}> <u> Todo List </u> </h4>
       </div>
     );
   }
 }
 
-function getList (a) {
+function moveOnClick(direction,key) {
+  if(direction === "l") {
+    console.log("Moved Left");
+    console.log(key);
+  } else if (direction === "r") {
+    console.log("Moved Right");
+    console.log(key);
+  }
+}
 
-  var x = a.map( a => (<ul key={a}>{a}</ul>));
-  console.log("Test : " + x);
-  return x;
+function getList (a,dir) {
+  if(dir === "l") {
+    var list = a.map( (var1,i,a) => (<ul key={"l" + i} onClick={() => moveOnClick("r","l"+i)}> {var1}</ul>));
+  } else if(dir === "r") {
+    var list = a.map( (var1,i,a) => (<ul key={"r" + i} onClick={() => moveOnClick("l","r"+i)}> {var1}</ul>));
+  }
+  return list;
 }
 
 class Table extends Component {
@@ -45,16 +58,19 @@ class Table extends Component {
       <div className="table-main">
         <table>
           <tbody>
-          <tr> 
-            <td className = "table-head"> <b> Tasks </b> </td>
-            <td className = "table-head"> <b> Completed </b> </td>
-          </tr>
+            <tr> 
+              <td className = "table-head"> <b> Tasks </b> </td>
+              <td className = "table-head"> <b> Completed </b> </td>
+            </tr>
 
-          <tr>
-            <td className = "table-list"> {getList(this.props.p)}
-            </td>
-            <td className = "table-list"> {getList(this.props.q)} </td>
-          </tr>
+            <tr>
+              <td className = "table-list" > 
+                { getList(this.props.listA,"l") }
+              </td>
+              <td className = "table-list" > 
+                { getList(this.props.listB,"r") }
+              </td>
+            </tr>
           </tbody>
         </table>
       </div>
@@ -63,6 +79,31 @@ class Table extends Component {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
